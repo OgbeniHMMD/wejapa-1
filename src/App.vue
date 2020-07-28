682711ac59af7dd1b1e6bf4a405e7a25ecc1ce01
Olajide A. Hammed <olajide.a.hammed@gmail.com>
2020-07-28 03:43:20 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <MyName name="Olajide, Hammed Abayomi"/>
   </div>
 </template>
-
+ 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyName
   }
 }
-</script>
+</script>  
 
-<style>
+<style> 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10em; 
 }
 </style>
+ 
---
+++ src/components/MyName.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName', 
+  props: {
+    name: String
+  }
+}
+</script> 
